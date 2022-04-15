44684744553dbbd1b220ff18bbb28414e7df53a3
hafizhuseynov <huseynoff.hafiz@gmail.com>
2022-04-15 18:11:10 +0400

    initial commit

--- Session 1/ViewController.swift
+++ Session 1/ViewController.swift
@@ -8,11 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      super.viewDidLoad()
+      self.view.backgroundColor = .blue
+      // Do any additional setup after loading the view.
+      
+      let label = UILabel()
+      label.frame = CGRect.init(x: 50, y: 50, width: 150, height: 150)
+      label.textInputMode()
+      label.text = "myFirstview"
+      label.textColor = .white
+      
+      label.backgroundColor = .red
+      self.view.addSubview(label)
     }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    test(value: "viewWillAppear")
+    self.test2("World")
+  }
+  
+  func test(value: String){
+    print(value)
+  }
+  
+  func test2(_ value: String){
+    print(value + " Hello")
+  }
 
 
 }
